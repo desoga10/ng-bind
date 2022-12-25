bd7c005dbaa6dd9d317dc9234e1641ec6e3ad377
desoga10 <done2stay@gmail.com>
2022-12-25 22:46:42 +0100

    Implemented Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'prop-binding';
+  isShowImage = false;
+  educativeLogo =
+    'https://res.cloudinary.com/dz4tt9omp/image/upload/v1671977557/educate.png';
+  angularLogo =
+    'https://res.cloudinary.com/dz4tt9omp/image/upload/v1671977926/angula.png';
+
+  imageToggler() {
+    this.isShowImage = !this.isShowImage;
+    console.log('this.isShowImage');
+  }
 }
